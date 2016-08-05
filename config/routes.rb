f78bde56b5a5e687427d8d6557a5a1c2534453ab
Lily Zhang <ellabailasola@gmail.com>
2016-08-05 12:25:49 -0400

    rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:new, :create]
+    resources :todos, only: [:create, :show, :update, :destroy]
+    resources :lists, only: [:create, :show, :update, :destroy]
+    resources :days, only:  [:create, :show]
+    resource :session, only: [:new, :create, :destroy]
+  end
+
 end
