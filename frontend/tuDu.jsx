2f907ef320a17cc159ba1d85d89115e05144c6d4
Lily Zhang <ellabailasola@gmail.com>
2016-08-05 11:23:05 -0400

    webpack

---
+++ frontend/tuDu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const TuDuApp = React.createClass({
+  render () {
+    return (
+
+    )
+  }
+})
+
+// export default
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<TuDuApp />, document.getElementById('main'));
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/tuDu.jsx",
+  output: {
+    path: "./app/assets/javascripts",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
