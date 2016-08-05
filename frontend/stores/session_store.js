f3720b5b0906a4ed85acae1adefbcc4d655d0a89
Peter Hechler <peter.hechler@gmail.com>
2016-08-05 16:12:04 -0400

    Implement react auth

---
+++ frontend/stores/session_store.js
@@ -0,0 +1,36 @@
+import Store from 'flux/utils'.Store
+import SessionConstants from '../constants/session_constants'
+import AppDispatcher from '../dispatcher/dispatcher'
+
+const SessionStore = new Store(AppDispatcher);
+
+let _currentUser;
+let _currentUserHasBeenFetched = false;
+
+SessionStore.currentUser = () {
+  return _currentUser;
+};
+
+SessionStore.isLoggedIn = () {
+  return !!_currentUser;
+};
+
+SessionStore.currentUserHasBeenFetched = () {
+  return _currentUserHasBeenFetched;
+};
+
+SessionStore.__onDispatch = (payload) {
+  switch (payload.actionType) {
+    case SessionConstants.CURRENT_USER_RECEIVED:
+      _currentUser = payload.currentUser;
+      _currentUserHasBeenFetched = true;
+			SessionStore.__emitChange();
+			break;
+		case SessionConstants.LOGOUT:
+			_currentUser = null;
+			SessionStore.__emitChange();
+			break;
+  }
+};
+
+export default SessionStore
