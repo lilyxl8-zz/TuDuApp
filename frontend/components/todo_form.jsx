8adde22aef285ff19964d705fbdc5fa6f31481c6
Lily Zhang <ellabailasola@gmail.com>
2016-08-08 17:19:16 -0400

    write todo_actions

---
+++ frontend/components/todo_form.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import TodoUtil from '../util/todo_util';
+
+const TodoForm = React.createClass({
+  getInitialState () {
+    return {
+      name: "";
+    }
+  },
+
+
+  updateName (e) {
+    this.setState({name: e.currentTarget.value});
+  },
+
+  executeSubmit (e) {
+    e.preventDefault();
+    TodoUtil.createTodo(this.state.name);
+  },
+
+  render () {
+    return (
+      <div>
+        <form className='todo-form' onSubmit={this.executeSubmit}>
+          <input
+            placeholder="mach es jetzt!"
+            value={this.state.name}
+            onChange={this.updateName} />
+        </form>
+      </div>
+    );
+  }
+})
+
+export default TodoForm
---
+++ frontend/util/todo_util.js
@@ -0,0 +1,18 @@
+import TodoActions from '../actions/todo_actions'
+
+const TodoUtil = {
+  createTodo: (name) => {
+    $.ajax({
+      type: 'POST',
+      url: '/api/todos',
+      dataType: 'json',
+      data: { name: name, list_id: 1 }, //TODO make lists a thing
+      success: (todo) => {
+        TodoActions.todoReceived(todo);
+      }
+    })
+  },
+
+}
+
+export default TodoUtil
