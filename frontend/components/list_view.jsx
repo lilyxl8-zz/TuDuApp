a734fbd48466efce92a1ecd85a48b77fe9ecdbf7
Lily Zhang <ellabailasola@gmail.com>
2016-08-29 16:38:20 -0400

    finish NameForm

--- frontend/components/list_view.jsx
+++ frontend/components/list_view.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ListUtil from '../utils/list_util';
 import ListStore from '../stores/list_store';
 import TodoList from './todo_list';
 import TodoForm from './todo_form';
@@ -8,9 +7,7 @@ import NameForm from './name_form';
 const ListView = React.createClass({
   getInitialState () {
     return {
-      list: this.props.list,
-      style: this.props.listStyle,
-      editing: false
+      style: this.props.listStyle
     };
   },
 
@@ -22,17 +19,6 @@ const ListView = React.createClass({
     this.setState({ list: this.props.list });
   },
 
-  updateName (e) {
-    let newList = this.state.list;
-    newList.name = e.currentTarget.value;
-    this.setState({ list: newList });
-  },
-
-  toggleEditing (e) {
-    e.preventDefault();
-    this.setState({ editing: !this.state.editing });
-  },
-
   focusTodoForm (e) {
     e.preventDefault();
     document.getElementById(this.props.list.id).firstChild.firstChild.focus();
@@ -42,23 +28,12 @@ const ListView = React.createClass({
     this.listStoreToken.remove();
   },
 
-  handleSubmit (e) {
-    e.preventDefault();
-    ListUtil.updateList(this.state.list);
-    this.toggleEditing(e);
-  },
-
-  deleteList (e) {
-    e.preventDefault();
-    ListUtil.deleteList(this.state.list);
-  },
-
   render () {
     let newTodo;
     let blankTodos = [];
 
-    if (this.state.list.todos.length < 10) {
-      for (let i = 0; i < 9 - this.state.list.todos.length; i++) {
+    if (this.props.list.todos.length < 10) {
+      for (let i = 0; i < 9 - this.props.list.todos.length; i++) {
         blankTodos.push(
           <div className='todo-item' key={i} onClick={this.focusTodoForm}></div>
         );
@@ -74,8 +49,7 @@ const ListView = React.createClass({
 
     return (
       <div className='list-view' style={ this.props.style }>
-        <NameForm editing={ this.state.editing }
-          name={ this.props.list.name } />
+        <NameForm list={this.props.list}/>
         <div className='list-todos'>
           <TodoList todos={ this.props.list.todos } />
           { newTodo }
--- frontend/components/name_form.jsx
+++ frontend/components/name_form.jsx
@@ -1,30 +1,54 @@
 import React from 'react';
+import ListUtil from '../utils/list_util';
 
 const NameForm = React.createClass({
   getInitialState () {
     return {
-      editing: this.props.editing
+      editing: false,
+      name: this.props.list.name
     };
+  },
+
+  updateName (e) {
+    e.preventDefault();
+    this.setState({ name: e.currentTarget.value });
+  },
+
+  toggleEditing (e) {
+    e.preventDefault();
+    this.setState({ editing: !this.state.editing });
+  },
+
+  handleSubmit (e) {
+    e.preventDefault();
+    this.props.list.name = this.state.name;
+    ListUtil.updateList(this.props.list);
+    this.toggleEditing(e);
+  },
+
+  deleteList (e) {
+    e.preventDefault();
+    ListUtil.deleteList(this.props.list);
   },
 
   render () {
     return (
       (this.state.editing) ? (
         <div className='list-name'>
-          <form className='name-form' onSubmit={this.handleSubmit}>
+          <form className='name-form' onSubmit={ this.handleSubmit }>
             <input
-              value={this.state.list.name}
-              onChange={this.updateName}
-              onBlur={this.toggleEditing}
+              value={ this.state.name }
+              onChange={ this.updateName }
+              onBlur={ this.toggleEditing }
               autoFocus />
           </form>
         </div>
       ) : (
         <div className='list-name'>
-          <h1 onClick={this.toggleEditing}>
-            {this.props.name}
+          <h1 onClick={ this.toggleEditing }>
+            { this.props.list.name }
           </h1>
-          <a onClick={this.deleteList} className='delete-list'></a>
+          <a onClick={ this.deleteList } className='delete-list'></a>
         </div>
       )
     );
