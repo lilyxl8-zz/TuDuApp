88bc993e6154518f008912fe422ff919b46d9865
Lily Zhang <ellabailasola@gmail.com>
2016-09-14 14:18:12 -0400

    fast-forward main.scss

--- frontend/tuDu.jsx
+++ frontend/tuDu.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import SessionUtil from './utils/session_util'
-import SessionStore from './stores/session_store'
+import SessionUtil from './utils/session_util';
+import SessionStore from './stores/session_store';
 
-import NavOverlay from './components/nav_overlay'
-import SignupForm from './components/signup_form'
-import SigninForm from './components/signin_form'
+import NavOverlay from './components/nav_overlay';
+import SignupForm from './components/signup_form';
+import SigninForm from './components/signin_form';
 
-import Landing from './components/landing'
-import AppIndex from './components/app_index'
+import Landing from './components/landing';
+import ListIndex from './components/list_index';
 
-import '../app/assets/stylesheets/main.scss'
+// require('../app/assets/stylesheets/main.scss');
 
 const TuDuApp = React.createClass({
   getInitialState () {
     return {
       currentUser: SessionStore.currentUser()
-    }
+    };
   },
 
   componentDidMount () {
-    SessionUtil.fetchCurrentUser()
-    this.sessionStoreToken = SessionStore.addListener(this._updateSession)
+    SessionUtil.fetchCurrentUser();
+    this.sessionStoreToken = SessionStore.addListener(this._updateSession);
   },
 
   _updateSession () {
-    this.setState({ currentUser: SessionStore.currentUser() })
+    this.setState({ currentUser: SessionStore.currentUser() });
   },
 
-  componentWillUnmount () {
-    this.sessionStoreToken.remove()
+  componentWillUnmount() {
+    this.sessionStoreToken.remove();
   },
 
   render () {
@@ -43,18 +43,18 @@ const TuDuApp = React.createClass({
         />
         { this.props.children }
       </div>
-    )
+    );
   }
-})
+});
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path= '/' component={ TuDuApp }>
-      <IndexRoute component={ Landing } />
-      <Route path= 'app' name='app' component={ AppIndex } />
-      <Route path= 'signup' name='signup' component={ SignupForm } />
-      <Route path='signin' name='signin' component={ SigninForm }/>
+    <Route path= '/' component={TuDuApp}>
+      <IndexRoute component={Landing} />
+      <Route path= 'app' name='app' component={ListIndex} />
+      <Route path= 'signup' name='signup' component={SignupForm} />
+      <Route path='signin' name='signin' component={SigninForm}/>
     </Route>
   </Router>,
   document.getElementById('main')
-)
+);
